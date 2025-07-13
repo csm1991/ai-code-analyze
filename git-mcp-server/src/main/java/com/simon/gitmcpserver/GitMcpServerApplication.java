669b4320dbec7a9a65023201a3adfa9ce43d20f6
caishumin <382087946@qq.com>
2025-07-14 00:15:05 +0800

    [master]fix git-mcp

--- git-mcp-server/src/main/java/com/simon/gitmcpserver/GitMcpServerApplication.java
+++ git-mcp-server/src/main/java/com/simon/gitmcpserver/GitMcpServerApplication.java
@@ -1,7 +1,11 @@
 package com.simon.gitmcpserver;
 
+import com.simon.gitmcpserver.service.GitService;
+import org.springframework.ai.tool.ToolCallbackProvider;
+import org.springframework.ai.tool.method.MethodToolCallbackProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GitMcpServerApplication {
@@ -10,4 +14,8 @@ public class GitMcpServerApplication {
         SpringApplication.run(GitMcpServerApplication.class, args);
     }
 
+    @Bean
+    public ToolCallbackProvider weatherTools(GitService gitService) {
+        return MethodToolCallbackProvider.builder().toolObjects(gitService).build();
+    }
 }
